f6d44fb884bcb774b11490fb01bb7f00f47d355c
sethhudson99 <118292769+sethhudson99@users.noreply.github.com>
2023-08-19 21:37:09 -1000

    calculator-demo

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -23,7 +23,7 @@ const Work = () => {
                             React Calculator
                         </span>
                         <div className='pt-8 text-center'>
-                            <a href="/">
+                            <a href="https://sethhudson99.github.io/calculator/">
                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                             </a>
                             <a href="https://github.com/sethhudson99/calculator.git">
