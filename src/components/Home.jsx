d5a1712dbc1ef249d378eb8d1561a01da82b52e0
sethhudson99 <118292769+sethhudson99@users.noreply.github.com>
2023-08-25 10:01:23 -1000

    web dev

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
         <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
             <p className=' text-[#fd0081]'>Hi, my name is</p>
             <h1 className='text-4xl sm:text-7xl font-bold text-gray-300'>Seth Hudson</h1>
-            <h2 className='text-4xl sm:text-7xl font-bold text-gray-400'>I'm a Frontend Developer.</h2>
+            <h2 className='text-4xl sm:text-7xl font-bold text-gray-400'>I'm a Web Developer.</h2>
             <p className=' text-gray-400 py-4 max-w-[700px]'>I am dedicated to developing solutions that result in best practices to deliver over the top user experience. I am seeking to further my knowledge and experience in web development.</p>
             <Link to="work" smooth={true} duration={500}>
               <div>
